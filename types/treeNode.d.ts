50021429e95d835183e8c0c3e5dd3deddb6bbe4e
Aion Chiu <aion.chiu@isobar.com>
2021-09-29 19:42:17 +0800

    remove old declare

--- types/treeNode.d.ts
+++
@@ -1,6 +0,0 @@
-﻿declare class TreeNode {
-    val: number
-    left: TreeNode | null
-    right: TreeNode | null
-    public constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null);
-}
