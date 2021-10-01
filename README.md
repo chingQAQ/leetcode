0d3ddd99b298172a2c1a16821d025c4bcdfaaa07
Aion Chiu <aion.chiu@isobar.com>
2021-10-01 17:38:50 +0800

    feat: subject 111.

--- README.md
+++ README.md
@@ -5,11 +5,13 @@
 | [101][0101] | [Symmetric Tree](<./src/101.symmetric-tree.ts>) | ![Easy][Easy] |
 | [108][0108] | [Convert Sorted Array to Binary Search Tree](<./src/108.convert-sorted-array-to-binary-search-tree.ts>) | ![Easy][Easy] |
 | [110][0110] | [Balanced Binary Tree](<./src/110.balanced-binary-tree.ts>) | ![Easy][Easy] |
+| [111][0111] | [Minimum Depth of Binary Tree](<./src/111.minimum-depth-of-binary-tree.ts>) | ![Easy][Easy] |
 
 [0094]: https://leetcode.com/problems/binary-tree-inorder-traversal/
 [0101]: https://leetcode.com/problems/symmetric-tree/
 [0108]: https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 [0110]: https://leetcode.com/problems/balanced-binary-tree/
+[0111]: https://leetcode.com/problems/minimum-depth-of-binary-tree/
 
 
 [Easy]: https://img.shields.io/badge/-Easy-brightgreen
---
+++ src/111.minimum-depth-of-binary-tree.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=111 lang=typescript
+ *
+ * [111] Minimum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+const minDepth = (root: TreeNode | null): number => {
+  if (root == null) return 0;
+
+  const depthDetect = (node: TreeNode | null): number => {
+    if (node == null) return 0;
+    if (node.left == null) return depthDetect(node.right) + 1;
+    if (node.right == null) return depthDetect(node.left) + 1;
+    return Math.min(depthDetect(node.left), depthDetect(node.right)) + 1;
+  };
+
+  return depthDetect(root);
+};
+
+module.exports = minDepth;
+// @lc code=end
+
