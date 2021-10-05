e1f3f0de7f1f051b7dd8f2be0efd511dd64f7544
Aion Chiu <aion.chiu@isobar.com>
2021-10-05 20:02:15 +0800

    stash

---
+++ __tests__/119.pascals-triangle-ii.spec.ts
@@ -0,0 +1,37 @@
+﻿const getRow = (rowIndex: number): number[] => {
+  
+};
+
+test('expect use rowIndex input 3, return [1,3,3,1].', () => {
+  const rowIndex: number = 3;
+  const expected = [1, 3, 3, 1];
+  const actual = getRow(rowIndex);
+
+  expect(actual).toEqual(expected);
+})
+
+test('expect use rowIndex input 0, return [1].', () => {
+  const rowIndex: number = 0;
+  const expected = [1];
+  const actual = getRow(rowIndex);
+
+  expect(actual).toEqual(expected);
+})
+
+test('expect use rowIndex input 1, return [1,1].', () => {
+  const rowIndex: number = 1;
+  const expected = [1, 1];
+  const actual = getRow(rowIndex);
+
+  expect(actual).toEqual(expected);
+})
+
+test('expect use rowIndex input 2, return [1,2,1].', () => {
+  const rowIndex: number = 2;
+  const expected = [1, 2, 1];
+  const actual = getRow(rowIndex);
+
+  expect(actual).toEqual(expected);
+})
+
+export default getRow;
---
+++ src/119.pascals-triangle-ii.ts
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=119 lang=typescript
+ *
+ * [119] Pascal's Triangle II
+ */
+
+// @lc code=start
+function getRow(rowIndex: number): number[] {
+
+};
+
+module.exports = getRow;
+// @lc code=end
+
